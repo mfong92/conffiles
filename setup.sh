4c311c735d6ff68b77bf83650d924681b7c0ea5f
Dan Wang <danwang@fb.com>
2012-05-21 21:41:49 -0700

    Adding a hacky script to create symlinks
    --danwang

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ln -s ~/conffiles/.aliases ~/.aliases
+ln -s ~/conffiles/.bash_profile ~/.bash_profile
+ln -s ~/conffiles/.bash_prompt ~/.bash_prompt
+ln -s ~/conffiles/.bashrc ~/.bashrc
+ln -s ~/conffiles/.berkeleyrc  ~/.berkeleyrc
+ln -s ~/conffiles/.dir_colors ~/.dir_colors
+ln -s ~/conffiles/.screenrc ~/.screenrc
+ln -s ~/conffiles/.vim ~/.vim
+ln -s ~/conffiles/.vimrc ~/.vimrc
