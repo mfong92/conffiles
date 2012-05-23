56ae421225bafe7cf55bb969b19a11cbe70cf220
Dan Wang <danwang@fb.com>
2012-05-22 20:54:25 -0700

    Renaming dotfiles to not be hidden, new script
    --danwang

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
-ln -s ~/conffiles/.aliases ~/.aliases
-ln -s ~/conffiles/.bash_profile ~/.bash_profile
-ln -s ~/conffiles/.bash_prompt ~/.bash_prompt
-ln -s ~/conffiles/.bashrc ~/.bashrc
-ln -s ~/conffiles/.berkeleyrc  ~/.berkeleyrc
-ln -s ~/conffiles/.dir_colors ~/.dir_colors
-ln -s ~/conffiles/.screenrc ~/.screenrc
-ln -s ~/conffiles/.vim ~/.vim
-ln -s ~/conffiles/.vimrc ~/.vimrc
+#!/bin/bash
+
+DIR=/Users/danwang/.dotfiles/
+
+mkdir -p ~/dotfiles_old
+
+cd $DIR
+for i in *
+do
+    ln -s $DIR/$i ~/.$i
+done
