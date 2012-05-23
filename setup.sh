7c92020ce624e1a154163ed6df3b483e90cef277
Dan Wang <danwang@fb.com>
2012-05-23 07:30:41 -0700

    Renaming .oh-my-zsh to oh-my-zsh and updating setup script
    --danwang

--- setup.sh
+++ setup.sh
@@ -3,13 +3,20 @@
 DIR=/Users/danwang/.dotfiles/
 
 mkdir -p ~/dotfiles_old
+find ~/dotfiles_old -type f -delete
 
 cd $DIR
 for i in *
 do
-    mv ~/.$i ~/dotfiles_old/
+    if [ -f ~/.$i ] ;then
+        if [ -f ~/dotfiles_old/.$i ] ;then
+            rm ~/dotfiles_old/.$i
+        fi
+        mv ~/.$i ~/dotfiles_old
+    fi
     ln -s $DIR/$i ~/.$i
 done
+
 
 rm ~/.setup.sh
 rm ~/.README.md
